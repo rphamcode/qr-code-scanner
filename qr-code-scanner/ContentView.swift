8f7ae8d8e94ec76974a9aeba42c07b8ba48d7171
Raul Pham <phatpham314@icloud.com>
2023-04-20 12:26:41 -0500

    Initial Commit

---
+++ qr-code-scanner/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  qr-code-scanner
+//
+//  Created by Pham on 4/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ qr-code-scanner/qr_code_scannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  qr_code_scannerApp.swift
+//  qr-code-scanner
+//
+//  Created by Pham on 4/20/23.
+//
+
+import SwiftUI
+
+@main
+struct qr_code_scannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
